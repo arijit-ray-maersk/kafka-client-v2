1c4f12ff97c06d1ab4bf613c903ad778e04949cd
arijit-ray-maersk <arijit.ray@lns.maersk.com>
2023-03-10 12:37:35 +0530

    Initial commit

---
+++ src/main/java/com/maersk/clients/kafkaclientv2/KafkaClientV2Application.java
@@ -0,0 +1,13 @@
+package com.maersk.clients.kafkaclientv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaClientV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaClientV2Application.class, args);
+	}
+
+}
